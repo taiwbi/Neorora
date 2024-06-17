0de1d25971b4ecb26a5c48d9d631f79ab2867565
taiwbi <admin@taiwbi.com>
2024-06-17 16:51:06 +0330

    feat: Added noice.nvim plugin and Removed laravel.nvim

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -91,21 +91,6 @@ return {
     opt = {},
   },
   {
-    "adalessa/laravel.nvim",
-    name = "laravel",
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-      "tpope/vim-dotenv",
-      "MunifTanjim/nui.nvim",
-    },
-    cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
-    event = { "VeryLazy" },
-    config = function()
-      require("laravel").setup()
-      require("telescope").load_extension "laravel"
-    end,
-  },
-  {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {},
@@ -133,5 +118,46 @@ return {
     "xiyaowong/transparent.nvim",
     name = "transparent",
     event = "VeryLazy",
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
+    opts = {
+      lsp = {
+        hover = {
+          enabled = false,
+          silent = false,
+          view = nil,
+          opts = {},
+        },
+        signature = {
+          enabled = false,
+          auto_open = {
+            enabled = true,
+            trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+            luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+            throttle = 50, -- Debounce lsp signature help request by 50ms
+          },
+          view = nil, -- when nil, use defaults from documentation
+          ---@type NoiceViewOptions
+          opts = {}, -- merged with defaults from documentation
+        },
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = false,
+        lsp_doc_border = false,
+      },
+    },
   },
 }
