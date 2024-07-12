83ec682ed3cc94bdccf697d3c92f82038b47a107
taiwbi <admin@taiwbi.com>
2024-07-12 18:14:33 +0330

    feat: Blade syntax higlighting
    - Blade syntax highlighting using blade-treesitter
    + Removed AstroNvim VSCode integration

--- lua/community.lua
+++ lua/community.lua
@@ -6,6 +6,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.recipes.vscode" },
+  -- { import = "astrocommunity.recipes.vscode" },
   -- import/override with your plugins folder
 }
