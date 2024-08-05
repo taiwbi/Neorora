69ca35408e5e9e27e4848801cf8f9e8a6bba5d1e
taiwbi <admin@taiwbi.com>
2024-08-05 18:27:30 +0330

    Back astrolsp.lua to be a object
    Function removed formatting abilities and I'm not gonna figure out how
    to fix that rn

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -6,17 +6,17 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrolsp",
-  ---@param opts AstroLSPOpts
-  opts = function(plugin, opts)
-    opts.features = {
+  ---@type AstroLSPOpts
+  opts = {
+    -- Configuration table of features provided by AstroLSP
+    features = {
       autoformat = true, -- enable or disable auto formatting on start
       codelens = true, -- enable/disable codelens refresh on start
       inlay_hints = false, -- enable/disable inlay hints on start
       semantic_tokens = true, -- enable/disable semantic token highlighting
-    }
-    -- Configuration table of features provided by AstroLSP
+    },
     -- customize lsp formatting options
-    opts.formatting = {
+    formatting = {
       -- control auto formatting on save
       format_on_save = {
         enabled = true, -- enable or disable format on save globally
@@ -35,10 +35,14 @@ return {
       -- filter = function(client) -- fully override the default formatting function
       --   return true
       -- end
-    }
+    },
+    -- enable servers that you already have installed without mason
+    servers = {
+      -- "pyright"
+    },
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
-    opts.config = {
+    config = {
       -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
       pyright = {
         root_dir = require("lspconfig.util").root_pattern "main.py",
@@ -54,10 +58,14 @@ return {
       },
       intelephense = {
         root_dir = function() return vim.loop.cwd() end,
-        filetypes = { "php" },
+        filetypes = { "blade", "php" },
         init_options = {
           licenceKey = "INTELEPHENSE_LICENSE_KEY", -- WARN: Add your intelephense license key here if you have one...
         },
+      },
+      stimulus_ls = {
+        root_dir = function() return vim.loop.cwd() end,
+        filetypes = { "blade" },
       },
       rust_analyzer = {
         settings = {
@@ -69,18 +77,18 @@ return {
           },
         },
       },
-    }
+    },
     -- customize how language servers are attached
-    opts.handlers = {
+    handlers = {
       -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
       -- function(server, opts) require("lspconfig")[server].setup(opts) end
 
       -- the key is the server that is being setup with `lspconfig`
       -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
       -- pyright = function(_, opts) require("lspconfig").pyright.setup(opts) end -- or a custom handler function can be passed
-    }
+    },
     -- Configure buffer local auto commands to add when attaching a language server
-    opts.autocmds = {
+    autocmds = {
       -- first key is the `augroup` to add the auto commands to (:h augroup)
       lsp_document_highlight = {
         -- Optional condition to create/delete auto command group
@@ -103,14 +111,11 @@ return {
           callback = function() vim.lsp.buf.clear_references() end,
         },
       },
-    }
+    },
     -- mappings to be set up on attaching of a language server
-    opts.mappings = {
+    mappings = {
       n = {
-        gl = {
-          function() vim.diagnostic.open_float() end,
-          desc = "Hover diagnostics",
-        },
+        gl = { function() vim.diagnostic.open_float() end, desc = "Hover diagnostics" },
         -- a `cond` key can provided as the string of a server capability to be required to attach, or a function with `client` and `bufnr` parameters from the `on_attach` that returns a boolean
         -- gD = {
         --   function() vim.lsp.buf.declaration() end,
@@ -123,12 +128,12 @@ return {
         --   cond = function(client) return client.server_capabilities.semanticTokensProvider and vim.lsp.semantic_tokens end,
         -- },
       },
-    }
+    },
     -- A custom `on_attach` function to be run after the default `on_attach` function
     -- takes two parameters `client` and `bufnr`  (`:h lspconfig-setup`)
-    opts.on_attach = function(client, bufnr)
+    on_attach = function(client, bufnr)
       -- this would disable semanticTokensProvider for all clients
       -- client.server_capabilities.semanticTokensProvider = nil
-    end
-  end,
+    end,
+  },
 }
