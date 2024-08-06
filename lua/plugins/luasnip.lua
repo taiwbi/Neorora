4e04094ba3ba1be1dcb94e8209296a2d5f209738
taiwbi <admin@taiwbi.com>
2024-08-06 20:50:39 +0330

    feat: Added livewire and custom blade.php snippets

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,14 @@
+return {
+  "L3MON4D3/LuaSnip",
+  config = function(plugin, opts)
+    -- include the default astronvim config that calls the setup call
+    require("astronvim.plugins.configs.luasnip")(plugin, opts)
+    -- load snippets paths
+    require("luasnip.loaders.from_vscode").lazy_load({
+      paths = { vim.fn.stdpath("config") .. "/snippets" },
+    })
+    print("Snippets loaded from path: " .. vim.fn.stdpath("config") .. "/snippets")
+
+    require("luasnip").filetype_extend("blade", {"html", "php"})
+  end,
+}
