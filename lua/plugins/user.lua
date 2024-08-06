a9219061fbeabd3d64be1a4ca91f9eae116d4215
taiwbi <admin@taiwbi.com>
2024-08-06 22:04:44 +0330

    refactor: Moved big plugin configuration blocks from user.lua to their specifc file

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -101,88 +101,9 @@ return {
     { import = "astrocommunity.pack.typescript" },
   },
   {
-    "jackMort/ChatGPT.nvim",
-    config = function()
-      require("chatgpt").setup {
-        openai_params = {
-          model = "gpt-4o",
-          frequency_penalty = 0,
-          presence_penalty = 0,
-          max_tokens = 4095,
-          temperature = 0.4,
-          top_p = 0.1,
-          n = 1,
-        },
-      }
-    end,
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "folke/trouble.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    cmd = {
-      "ChatGPT",
-      "ChatGPTActAs",
-      "ChatGPTEditWithInstructions",
-      "ChatGPTRun",
-      "ChatGPTCompleteCode",
-    },
-    keys = {
-      { "<Leader>ac", function() vim.cmd "ChatGPTCompleteCode" end, desc = "Get code suggestion from GPT" },
-    },
-  },
-  {
     "2giosangmitom/nightfall.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
-  },
-  {
-    -- Configuration in /lua/plugins/astrocore.lua file
-    "xiyaowong/transparent.nvim",
-    name = "transparent",
-    event = "User AstroFile",
-  },
-  {
-    "folke/noice.nvim",
-    event = "User AstroFile",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "rcarriga/nvim-notify",
-    },
-    opts = {
-      lsp = {
-        hover = {
-          enabled = false,
-          silent = false,
-          view = nil,
-          opts = {},
-        },
-        signature = {
-          enabled = false,
-          auto_open = {
-            enabled = true,
-            trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
-            luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
-            throttle = 50, -- Debounce lsp signature help request by 50ms
-          },
-          view = nil, -- when nil, use defaults from documentation
-          ---@type NoiceViewOptions
-          opts = {}, -- merged with defaults from documentation
-        },
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-        },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = false,
-        lsp_doc_border = false,
-      },
-    },
   },
 }
