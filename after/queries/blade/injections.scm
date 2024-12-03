a0322e4186c95f633b93a1efb0b830800ef657a5
taiwbi <admin@taiwbi.com>
2024-12-03 16:22:47 +0330

    feat: Updated Laravel Blade Syntax Queries

--- after/queries/blade/injections.scm
+++ after/queries/blade/injections.scm
@@ -3,10 +3,10 @@
     (#set! injection.combined)
     (#set! injection.language php))
 
-((text) @injection.content
-    (#not-has-ancestor? @injection.content "envoy")
-    (#set! injection.combined)
-    (#set! injection.language php))
+; tree-sitter-comment injection
+; if available
+((comment) @injection.content
+ (#set! injection.language "comment"))
 
 ; could be bash or zsh
 ; or whatever tree-sitter grammar you have.
@@ -15,8 +15,9 @@
     (#set! injection.combined)
     (#set! injection.language bash))
 
-; 🚧  Available for experimental split_parser see issue #5 
 ((php_only) @injection.content
-   (#set! injection.language php_only))
-((parameter) @injection.content
-   (#set! injection.language php_only))
+    (#set! injection.language php_only))
+
+((parameter) @injection.content                                                                                                 
+    (#set! injection.include-children) ; You may need this, depending on your editor e.g Helix                                                                                          
+    (#set! injection.language "php-only"))
