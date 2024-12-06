703188e58e86d87d6e2811560ddf4d379169924a
taiwbi <admin@taiwbi.com>
2024-12-06 16:30:24 +0330

    chore: Configured Neovide for better animations :)

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -8,10 +8,8 @@ return {
   opts = {
     options = {
       opt = { -- configure vim.opt options
-        -- configure font
-        guifont = "Monaspace Neon,AzarMehrMonospaced:h11",
         -- line spacing
-        linespace = 0,
+        linespace = 2,
         guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175",
         winblend = 0,
         pumblend = 0,
@@ -24,16 +22,16 @@ return {
         neovide_padding_bottom = 0,
         neovide_padding_right = 0,
         neovide_padding_left = 0,
-        neovide_transparency = 100,
+        neovide_transparency = 0.9,
         neovide_floating_blur_amount_x = 0,
         neovide_floating_blur_amount_y = 0,
         neovide_floating_shadow = false,
         neovide_floating_z_height = 10,
         neovide_light_angle_degrees = 45,
         neovide_light_radius = 5,
-        neovide_scroll_animation_length = 0.1,
-        neovide_cursor_animation_length = 0.1,
-        neovide_cursor_trail_size = 0.2,
+        neovide_scroll_animation_length = 0.8,
+        neovide_cursor_animation_length = 0.2,
+        neovide_cursor_trail_size = 0.02,
         neovide_hide_mouse_when_typing = true,
         neovide_cursor_smooth_blink = true,
         neovide_cursor_vfx_mode = "pixiedust",
