a7fd123cb1d0e541efd31d4a753accf3d622a2ba
taiwbi <taiwbii@proton.me>
2025-04-05 08:40:16 +0330

    feat: XML treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,23 +2,24 @@
 
 ---@type LazySpec
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = {
-      "lua",
-      "vim",
-      "sql",
-      "bash",
-      "html",
-      "css",
-      "javascript",
-      "php",
-      "blade",
-      "scss",
-      "python",
-      "rust",
-      "hyprlang",
-      "diff",
-    },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	opts = {
+		ensure_installed = {
+			"lua",
+			"vim",
+			"sql",
+			"bash",
+			"html",
+			"css",
+			"javascript",
+			"php",
+			"blade",
+			"scss",
+			"python",
+			"rust",
+			"hyprlang",
+			"diff",
+			"xml",
+		},
+	},
 }
