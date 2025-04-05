9ee70aa9d9d9bc4c306b8ce8d710a8b3233e8c13
taiwbi <taiwbii@proton.me>
2025-04-05 08:40:05 +0330

    feat: Configured neovide for Astro V5

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -1,44 +1,43 @@
 if not vim.g.neovide then
-  return {} -- do nothing if not in a Neovide session
+	return {} -- do nothing if not in a Neovide session
 end
 
 return {
-  "AstroNvim/astrocore",
-  ---@type AstroCoreOpts
-  opts = {
-    options = {
-      opt = { -- configure vim.opt options
-        -- line spacing
-        linespace = 6,
-        guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175",
-        winblend = 0,
-        pumblend = 0,
-      },
-      g = { -- configure vim.g variables
-        -- configure scaling
-        neovide_scale_factor = 1.0,
-        -- configure padding
-        neovide_padding_top = 0,
-        neovide_padding_bottom = 0,
-        neovide_padding_right = 0,
-        neovide_padding_left = 0,
-        neovide_opacity = 1,
-        neovide_floating_blur_amount_x = 8,
-        neovide_floating_blur_amount_y = 8,
-        neovide_floating_shadow = false,
-        neovide_floating_z_height = 3,
-        neovide_light_angle_degrees = 45,
-        neovide_light_radius = 3,
-        neovide_floating_corner_radius = 12.0,
-        neovide_scroll_animation_length = 0.5,
-        neovide_cursor_animation_length = 0.5,
-        neovide_cursor_trail_size = 0.1,
-        neovide_hide_mouse_when_typing = true,
-        neovide_cursor_smooth_blink = true,
-        neovide_cursor_vfx_mode = "pixiedust",
-        neovide_cursor_vfx_particle_lifetime = 5,
-        neovide_cursor_vfx_particle_density = 30,
-      },
-    },
-  },
+	"AstroNvim/astrocore",
+	---@type AstroCoreOpts
+	opts = {
+		options = {
+			opt = { -- configure vim.opt options
+				-- line spacing
+				linespace = 2,
+				guifont = "Maple Mono:h11.1",
+				guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175",
+				winblend = 0,
+				pumblend = 0,
+			},
+			g = { -- configure vim.g variables
+				-- configure padding
+				neovide_padding_top = 7,
+				neovide_padding_bottom = 7,
+				neovide_padding_right = 0,
+				neovide_padding_left = 0,
+				neovide_opacity = 1,
+				neovide_floating_blur_amount_x = 8,
+				neovide_floating_blur_amount_y = 8,
+				neovide_floating_shadow = true,
+				neovide_floating_z_height = 3,
+				neovide_light_angle_degrees = 45,
+				neovide_light_radius = 3,
+				neovide_floating_corner_radius = 48.0,
+				neovide_scroll_animation_length = 0.3,
+				neovide_cursor_animation_length = 0.3,
+				neovide_cursor_trail_size = 0.07,
+				neovide_hide_mouse_when_typing = true,
+				neovide_cursor_smooth_blink = true,
+				neovide_cursor_vfx_mode = "pixiedust",
+				neovide_cursor_vfx_particle_lifetime = 2,
+				neovide_cursor_vfx_particle_density = 10,
+			},
+		},
+	},
 }
