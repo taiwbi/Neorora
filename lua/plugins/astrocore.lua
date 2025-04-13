e6fe9269eb172a56a45c29dc8579d7d80d7bd3b3
taiwbi <taiwbii@proton.me>
2025-04-13 16:25:58 +0330

    fix: Removed extra example in keybindings

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -64,16 +64,6 @@ return {
         -- Navigate buffer tabs
         ["<Tab>"] = { function() require("astrocore.buffer").nav(vim.v.count1) end, desc = "Next buffer" },
         ["<S-Tab>"] = { function() require("astrocore.buffer").nav(-vim.v.count1) end, desc = "Previous buffer" },
-
-        -- Buffer
-        ["<Leader>bd"] = {
-          function()
-            require("astroui.status.heirline").buffer_picker(
-              function(bufnr) require("astrocore.buffer").close(bufnr) end
-            )
-          end,
-          desc = "Close buffer from tabline",
-        },
       },
     },
   },
