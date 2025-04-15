2052c9b6618608b26a13aa0eca1ee2beb20b4454
taiwbi <taiwbii@proton.me>
2025-04-15 08:23:06 +0330

    style: Iosevka font

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -9,8 +9,8 @@ return {
     options = {
       opt = { -- configure vim.opt options
         -- line spacing
-        linespace = 0,
-        guifont = "Maple Mono:h12",
+        linespace = 2,
+        guifont = "Iosevka Term:h12",
         guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175",
         winblend = 0,
         pumblend = 0,
