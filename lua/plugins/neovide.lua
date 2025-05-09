2d6ab81a82ed5ebcf546fe51245b78f1450cbf77
taiwbi <taiwbii@proton.me>
2025-05-09 15:09:36 +0330

    style(styles): Adjust Neovide font size and line spacing
    - Increase line spacing from 3 to 7
    - Decrease font size from h12 to h11

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -9,8 +9,8 @@ return {
     options = {
       opt = { -- configure vim.opt options
         -- line spacing
-        linespace = 3,
-        guifont = "Geist Mono,:h12",
+        linespace = 7,
+        guifont = "Geist Mono,:h11",
         guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175",
         winblend = 0,
         pumblend = 0,
