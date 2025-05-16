06835a72864fe84a66b9d50ee37bff786a7d23c7
taiwbi <taiwbii@proton.me>
2025-05-16 22:42:38 +0330

    feat: Replaced avante with CodeCompanion

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,42 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = false, -- Never set this value to "*"! Never!
+  opts = {
+    provider = "gemini",
+    gemini = {
+      model = "gemini-2.5-flash-preview-04-17",
+      api_key_name = "GEMINI_KEY",
+      timeout = 60000,
+      temperature = 0.25,
+      generationConfig = {
+        responseMimeType = "text/plain",
+      },
+    },
+  },
+  build = "make",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    -- "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "echasnovski/mini.pick",
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+  },
+}
