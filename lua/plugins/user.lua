a1e05873bd86a915d186a5c6b46c257c7931138b
taiwbi <taiwbii@proton.me>
2025-07-14 10:07:06 +0330

    chore: Oxocarbon theme

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -44,17 +44,6 @@ return {
     opts = {},
   },
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      color_overrides = {
-        mocha = {
-          base = "#1a1a1a",
-          mantle = "#161616",
-          crust = "#0f0f0f",
-        },
-      },
-    },
+    "nyoom-engineering/oxocarbon.nvim",
   },
 }
