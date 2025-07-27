d054fe11b940e8949447b7b641458e9558d982ca
taiwbi <taiwbii@proton.me>
2025-07-27 09:59:23 +0330

    style(styles): update colorscheme and Neovide visual settings
    - Switch from oxocarbon to rose-pine colorscheme
    - Increase Neovide line spacing from 4 to 8
    - Remove Neovide top padding

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -44,6 +44,8 @@ return {
     opts = {},
   },
   {
-    "nyoom-engineering/oxocarbon.nvim",
+    "rose-pine/neovim",
+    name = "rose-pine",
+    config = function() vim.cmd "colorscheme rose-pine" end,
   },
 }
