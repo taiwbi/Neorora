a2cb0f5ece3558823d251b30354501fc786d13ed
taiwbi <taiwbii@proton.me>
2025-08-29 09:56:21 +0330

    chore: removed latest projects in dashboard

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -27,7 +27,7 @@ return {
           padding = 2,
         },
         { pane = 1, section = "header" },
-        { pane = 1, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        -- { pane = 1, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
         -- { pane = 2, section = "keys", gap = 1, padding = 1 },
         -- { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
         -- { pane = 2, section = "startup" },
