1aae8fa2a833c44b0debe8c2f720ed66a93e9675
taiwbi <taiwbii@proton.me>
2025-12-03 09:36:00 +0330

    ✨ feat(ui/ux): Revamp dashboard layout and content
    - Remove large ASCII art header.
    - Enable keybindings, projects, recent files, and startup sections.
    - Update dashboard image to vigil.png and adjust dimensions.
    - Rename existing asset images for consistency.
    - Add new virtuous.png asset.

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -4,33 +4,25 @@ return {
     image = { doc = { enabled = true } },
     dashboard = {
       preset = {
-        header = [[
-
-█░█ █▀▀ █▀█   █▀█ ▄▀█ █▀▄ █ ▄▀█ █▄░█ ▀█▀   █▀█ █▀█ █ █▀ █▀▀   █▀█ █▀▀ █▀ █▀█ █░░ █░█ █▀▀
-█▀█ ██▄ █▀▄   █▀▄ █▀█ █▄▀ █ █▀█ █░▀█ ░█░   █▀▀ █▄█ █ ▄█ ██▄   █▀▄ ██▄ ▄█ █▄█ █▄▄ ▀▄▀ ██▄
-
-█▀▀ ▄▀█ █▀█ ▀█▀ █ █░█ ▄▀█ ▀█▀ █▀▀   █▀▀ █▄░█ █▀▄ █░░ █▀▀ █▀ █▀ █░░ █▄█ ░
-█▄▄ █▀█ █▀▀ ░█░ █ ▀▄▀ █▀█ ░█░ ██▄   ██▄ █░▀█ █▄▀ █▄▄ ██▄ ▄█ ▄█ █▄▄ ░█░ ▄
-]],
-        -- keys = {
-        --   { key = "n", action = "<Leader>n", icon = "", desc = "New File  " },
-        --   { key = "o", action = "<Leader>fo", icon = "󰈙", desc = "Recents  " },
-        --   { key = "s", action = "<Leader>Sl", icon = "", desc = "Last Session  " },
-        -- },
+        keys = {
+          { key = "n", action = "<Leader>n", icon = "", desc = "New File  " },
+          { key = "o", action = "<Leader>fo", icon = "󰈙", desc = "Recents  " },
+          { key = "s", action = "<Leader>Sl", icon = "", desc = "Last Session  " },
+        },
       },
       sections = {
         {
           pane = 1,
           section = "image",
-          path = "~/.config/nvim/assets/LookDown.png",
-          height = 12,
-          padding = 2,
+          path = "~/.config/nvim/assets/vigil.png",
+          height = 9,
+          padding = 1,
         },
-        { pane = 1, section = "header" },
-        -- { pane = 1, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-        -- { pane = 2, section = "keys", gap = 1, padding = 1 },
-        -- { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-        -- { pane = 2, section = "startup" },
+        { pane = 1, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        { pane = 2, title = "\n\n\n\n\n" },
+        { pane = 2, section = "keys", gap = 0, padding = 1 },
+        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        { pane = 2, section = "startup" },
       },
     },
   },
