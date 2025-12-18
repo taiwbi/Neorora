7d6a58369e41867bf21f37f21afedc8af09249d7
taiwbi <taiwbii@proton.me>
2025-12-18 22:50:06 +0330

    ✨ feat(vscode): add VSextensions command and extension keybindings
    - Add VSextensions command to open the extensions view.
    - Add keybindings for Todo Tree, Docker, GitLens, and Debug views.
    - Add keybinding for MySQL extension.

--- lua/plugins/vscode.lua
+++ lua/plugins/vscode.lua
@@ -1,6 +1,8 @@
 -- don't do anything in non-vscode instances
 if not vim.g.vscode then return {} end
 
+vim.api.nvim_create_user_command("VSextensions", function() vim.fn.VSCodeNotify "workbench.view.extensions" end, {})
+
 ---@type LazySpec
 return {
   -- add a few keybindings
@@ -22,6 +24,12 @@ return {
           ["]s"] = "<CMD>call VSCodeNotify('cSpell.goToNextSpellingIssue')<CR>",
           ["[s"] = "<CMD>call VSCodeNotify('cSpell.goToPreviousSpellingIssue')<CR>",
 
+          ["<Leader>fT"] = "<CMD>call VSCodeNotify('workbench.view.extension.todo-tree-container')<CR>",
+          ["<Leader>fd"] = "<CMD>call VSCodeNotify('workbench.view.extension.containersView')<CR>",
+
+          ["<Leader>gG"] = "<CMD>call VSCodeNotify('workbench.view.extension.gitlens')<CR>",
+          ["<Leader>gI"] = "<CMD>call VSCodeNotify('workbench.view.extension.gitlensInspect')<CR>",
+
           ["<Leader>th"] = "<CMD>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>",
           ["<Leader>tv"] = "<CMD>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>",
           ["<Leader>tf"] = "<CMD>call VSCodeNotify('workbench.action.terminal.toggleTerminal')<CR>",
@@ -31,6 +39,10 @@ return {
           ["<leader>ld"] = "<Cmd>call VSCodeNotify('editor.action.showHover')<CR>",
 
           ["<leader>W"] = "<CMD>call VSCodeNotify('workbench.action.files.saveWithoutFormatting')<CR>",
+
+          ["<leader>du"] = "<CMD>call VSCodeNotify('workbench.view.debug)<CR>",
+
+          ["<leader>Db"] = "<CMD>call VSCodeNotify('workbench.view.extension.github-cweijan-mysql')<CR>",
 
           ["<leader>ag"] = "<CMD>call VSCodeNotify('antigravity.openAgent')<CR>", -- Open Agent
           ["<leader>ae"] = "<CMD>call VSCodeNotify('antigravity.prioritized.command.open')<CR>", -- Open AI quick edit
