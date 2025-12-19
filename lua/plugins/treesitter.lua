43fe52387df77c610997d136ed9cd6f36def18b2
taiwbi <taiwbii@proton.me>
2025-12-19 15:31:22 +0330

    🔧 chore(config): remove blade parser from treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,16 +4,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = function(_, opts)
-    local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-    parser_config.blade = {
-      install_info = {
-        url = "https://github.com/EmranMR/tree-sitter-blade",
-        files = { "src/parser.c" },
-        branch = "main",
-      },
-      filetype = "blade",
-    }
-
     if not opts.ensure_installed then opts.ensure_installed = {} end
     vim.list_extend(opts.ensure_installed, {
       "lua",
@@ -24,7 +14,6 @@ return {
       "css",
       "javascript",
       "php",
-      "blade",
       "scss",
       "python",
       "rust",
