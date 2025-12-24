1b77448fcb17be23b32cada555e6f6d61f476494
taiwbi <taiwbii@proton.me>
2025-12-24 14:47:07 +0330

    🔧 chore(config): remove lazygit fullscreen keybinding
    - Remove the <leader>gg mapping that opens lazygit via toggleterm.
    - Transition to using an external lazygit configuration file.

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -89,23 +89,6 @@ return {
         ["<leader>jl"] = { function() require("vstask").launches() end, desc = "Launch Configs" },
         ["<leader>jr"] = { function() require("vstask").command() end, desc = "Run Command" },
 
-        -- Lazygit
-        ["<leader>gg"] = {
-          function()
-            local toggleterm = require "toggleterm.terminal"
-            local lazygit = toggleterm.Terminal:new {
-              cmd = "lazygit -sm full",
-              hidden = true,
-              direction = "float",
-              float_opts = {
-                border = "rounded",
-              },
-            }
-            lazygit:toggle()
-          end,
-          desc = "ToggleTerm lazygit (fullscreen)",
-        },
-
         -- Paste using Ctrl+Shift+V
         ["<C-S-v>"] = { '"+p', desc = "Paste from clipboard" },
 
