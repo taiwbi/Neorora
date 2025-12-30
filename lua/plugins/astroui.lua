262fc778fa5fd8ad5199c614c22f484cd3b4cc7b
taiwbi <taiwbii@proton.me>
2025-12-30 19:37:01 +0330

    chore(ui): Adwaita as default theme

--- lua/plugins/astroui.lua
+++ lua/plugins/astroui.lua
@@ -7,7 +7,7 @@ return {
   ---@type AstroUIOpts
   opts = {
     -- change colorscheme
-    colorscheme = "kanagawa-wave",
+    colorscheme = "adwaita",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
     highlights = {
       init = { -- this table overrides highlights in all themes
