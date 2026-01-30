d6a85585c2dfb6eca1bc6b7cf48f06428a5527ce
taiwbi <taiwbii@proton.me>
2026-01-30 12:31:27 +0330

    💄 style(neovide): adjust line spacing and cursor particle settings
    - Reduce linespace from 17 to 9
    - Decrease cursor particle lifetime and density for a more subtle effect

--- lua/plugins/neovide.lua
+++ lua/plugins/neovide.lua
@@ -9,7 +9,7 @@ return {
     options = {
       opt = { -- configure vim.opt options
         -- line spacing
-        linespace = 17,
+        linespace = 9,
         guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175",
         winblend = 0,
         pumblend = 0,
@@ -34,8 +34,8 @@ return {
         neovide_hide_mouse_when_typing = true,
         neovide_cursor_smooth_blink = true,
         neovide_cursor_vfx_mode = "pixiedust",
-        neovide_cursor_vfx_particle_lifetime = 5,
-        neovide_cursor_vfx_particle_density = 25,
+        neovide_cursor_vfx_particle_lifetime = 3,
+        neovide_cursor_vfx_particle_density = 5,
       },
     },
   },
