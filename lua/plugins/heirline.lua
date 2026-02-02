3f2d237e7d2dcd137a4c4c7f432f281923688386
taiwbi <taiwbii@proton.me>
2026-02-02 22:31:47 +0330

    ✨ feat(statusline): improve status line appearance and minimalism
    - Add VimIcon to astroui symbols
    - Replace path and breadcrumbs separators with new rounded separators
    - Simplify statusline configuration: remove redundant components
    - Add new icon and separator for mode component
    - Reorder components for cleaner look

--- lua/plugins/heirline.lua
+++ lua/plugins/heirline.lua
@@ -4,35 +4,28 @@ return {
     local status = require "astroui.status"
 
     opts.winbar = nil
-    opts.statusline = { -- statusline
+    opts.statusline = {
       hl = { fg = "fg", bg = "bg" },
       status.component.mode {
-        mode_text = { padding = { left = 1, right = 1 } },
-      },
-      status.component.git_branch(),
-      status.component.builder {
-        {
-          provider = function() return "󱑒 " .. os.date "%H:%M" .. " " end,
+        mode_text = {
+          icon = { kind = "VimIcon", padding = { right = 1, left = 1 } },
         },
+        surround = { separator = "rounded_right" },
       },
       status.component.file_info {
         filename = {},
         filetype = false,
       },
-      status.component.git_diff(),
-      status.component.builder {
-        padding = { left = 0 },
-      },
-      status.component.breadcrumbs { icon = { enabled = false } },
       status.component.fill(),
+      status.component.git_diff(),
       status.component.diagnostics(),
-      status.component.virtual_env(),
-      status.component.cmd_info(),
       status.component.nav {
         scrollbar = false,
         padding = { right = 1 },
       },
-      status.component.mode { surround = { separator = "right" } },
+      status.component.mode {
+        surround = { separator = "rounded_left" },
+      },
     }
   end,
 }
