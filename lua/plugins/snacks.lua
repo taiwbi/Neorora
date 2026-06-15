0d6f0226ddd796519863b740b0c77fc733de67b3
taiwbi <taiwbii@proton.me>
2026-06-15 19:42:52 +0330

    refactor(snacks): simplify dashboard layout
    - Remove ASCII art header and replace with text
    - Remove dashboard keybindings
    - Adjust image dimensions and switch to a single-pane layout

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -41,48 +41,21 @@ return {
     },
     dashboard = {
       preset = {
-        keys = {
-          { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-          { icon = " ", key = "g", desc = "Recent Projects", action = "<leader>Sf" },
-          { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = "", key = "s", desc = "Last Session", action = "<Leader>Sl" },
-          {
-            icon = " ",
-            key = "c",
-            desc = "Config",
-            action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-          },
-          { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy" },
-          { icon = " ", key = "M", desc = "Mason", action = ":Mason" },
-          { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-        },
-        header = [[
- ▄▄    ▄ ▄▄▄▄▄▄▄ ▄▄▄▄▄▄▄ ▄▄   ▄▄ ▄▄▄ ▄▄   ▄▄
-█  █  █ █       █       █  █ █  █   █  █▄█  █
-█   █▄█ █    ▄▄▄█   ▄   █  █▄█  █   █       █
-█       █   █▄▄▄█  █ █  █       █   █       █
-█  ▄    █    ▄▄▄█  █▄█  █       █   █       █
-█ █ █   █   █▄▄▄█       ██     ██   █ ██▄██ █
-█▄█  █▄▄█▄▄▄▄▄▄▄█▄▄▄▄▄▄▄█ █▄▄▄█ █▄▄▄█▄█   █▄█
-
-One day, you will :q for the last time. Make
-today's commits count.
-        ]],
+        header = [[One day, you will :q for the last time. Make
+today's commits count.]],
       },
       sections = {
         {
           pane = 1,
           section = "image",
           path = "~/.config/nvim/assets/virtuous.png",
-          height = 15,
-          width = 45,
+          height = 10,
+          width = 27,
           padding = 2,
         },
+        { pane = 1, section = "header", padding = 2 },
         { pane = 1, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
         { pane = 1, section = "startup" },
-        { pane = 2, padding = 2 },
-        { pane = 2, section = "header", padding = 2 },
-        { pane = 2, section = "keys" },
       },
     },
   },
